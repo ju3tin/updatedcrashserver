5c38453c7e7af0a8c10ced8b639416843c59189c
ju3tin <ju3tin@hotmail.co.uk>
2025-11-24 13:26:32 +0000

    sdf

--- app1.js
+++ app1.js
@@ -129,7 +129,7 @@ function tickGame() {
   const target = 1 + (elapsed / 10);
   currentRound.multiplier = Math.min(target * 8, currentRound.crashPoint + 0.05);
 
-  broadcast('tick', {
+  broadcast('CNT_MULTIPLY', {
     multiplier: parseFloat(currentRound.multiplier.toFixed(2)),
     vault: displayedVault
   });
